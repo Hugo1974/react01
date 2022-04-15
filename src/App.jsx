30eefe0be05dde2de2110d5ae916834174cdad3a
Hugo1974 <h.morago.martin@gmail.com>
2022-04-15 22:56:30 +0200

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react';
 import './App.css';
 import CompraElements from './components/compraElements';
 import { hola } from './packages/funciones';
+<<<<<<< Updated upstream
+=======
+import uuid from 'react-uuid';
+
 
 function App() {
   const [count, setCount] = useState(0);
+
+>>>>>>> Stashed changes
+
+
+  
+function App() {
+  const [count, setCount] = useState(0);
+<<<<<<< Updated upstream
   hola("Mundo cruel");
+=======
+  hola('Mundo cruel');
+
+>>>>>>> Stashed changes
   return (
     <div className='App'>
       <CompraElements />
